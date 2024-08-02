6f846f1918b19706b361e9bf8e7e3be54800306f
Andrew <andrewchau1993@gmail.com>
2024-08-02 09:02:38 -0700

    commit for help

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -1,16 +1,38 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const User = require('../models/user.js')
-const Recipe = require('../models/recipe.js')
+const User = require("../models/user.js");
+const Recipe = require("../models/recipe.js");
+
+//ALL routes should start with /recipe/
 
 // GET index
-router.get('/', (req,res) => {
-  res.render('recipes/index.ejs')
-})
+router.get("/", (req, res) => {
+  try {
+    res.render("recipes/index.ejs");
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-router.get('/new', (req,res) => {
-  res.render('/recipes/new.ejs')
-})
+router.get("/new", (req, res) => {
+  try {
+    res.render("recipes/new.ejs");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const newRecipe = new Recipe(req.body);
+    newRecipe.owner = req.session.user._id
+    newRecipe.ingredients = objectId
+    await newRecipe.save();
+    res.redirect('/recipes')
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 module.exports = router;
