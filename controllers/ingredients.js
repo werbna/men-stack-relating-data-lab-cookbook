0444257d5ede6a423697928082b1cb100d4a682d
Andrew <andrewchau1993@gmail.com>
2024-08-02 17:12:00 -0700

    set up routes

--- controllers/ingredients.js
+++ controllers/ingredients.js
@@ -4,6 +4,35 @@ const router = express.Router()
 const User = require('../models/user.js')
 const Recipe = require('../models/recipe.js')
 
+router.get("/", async (req, res) => {
+  try {
+    const userId = req.session.user._id
+    const recipes = await Recipe.find({ owner: userId})
+    res.render("recipes/index.ejs", { recipes} );
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get("/new", (req, res) => { 
+  try {
+    res.render('recipes/new.ejs');
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.post("/", async (req, res) => {
+  console.log(req.body)
+  try {
+    const newIngredient = new Ingredient(req.body);
+    newIngredient.owner = req.session.user._id;
+    await newIngredient.save();
+    res.redirect('/recipes')
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 
 module.exports = router;
